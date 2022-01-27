f95c120f13d5be9ad4fc03076cf4a35247dc496b
Darkmanti <krivenko_102@mail.ru>
2022-01-27 17:00:05 +0300

    snap back to two separate blend state

--- render/BlendState.cpp
+++ render/BlendState.cpp
@@ -23,17 +23,40 @@ ComPtr<ID3D11BlendState> MakeAlphaBS() {
 
 	// without alpha [1]
 
-	state_desc.RenderTarget[1].BlendEnable = true;
+	/*state_desc.RenderTarget[1].BlendEnable = true;
 	state_desc.RenderTarget[1].SrcBlend = D3D11_BLEND_SRC_COLOR;
 	state_desc.RenderTarget[1].DestBlend = D3D11_BLEND_INV_SRC_COLOR;
 	state_desc.RenderTarget[1].BlendOp = D3D11_BLEND_OP_ADD;
 	state_desc.RenderTarget[1].SrcBlendAlpha = D3D11_BLEND_ONE;
 	state_desc.RenderTarget[1].DestBlendAlpha = D3D11_BLEND_ZERO;
 	state_desc.RenderTarget[1].BlendOpAlpha = D3D11_BLEND_OP_ADD;
-	state_desc.RenderTarget[1].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	state_desc.RenderTarget[1].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;*/
 
 	ComPtr<ID3D11BlendState> state;
 	renderer->GetDevice()->CreateBlendState(&state_desc, &state);
 
 	return state;
 };
+
+ComPtr<ID3D11BlendState> MakeNoAlphaBS() {
+	Renderer* renderer = Renderer::GetInstance();
+
+	D3D11_BLEND_DESC state_desc;
+	ZeroMemory(&state_desc, sizeof(D3D11_BLEND_DESC));
+	state_desc.AlphaToCoverageEnable = false;
+	state_desc.IndependentBlendEnable = false;
+
+	state_desc.RenderTarget[0].BlendEnable = true;
+	state_desc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_COLOR;
+	state_desc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_COLOR;
+	state_desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+	state_desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+	state_desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+	state_desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	state_desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+
+	ComPtr<ID3D11BlendState> state;
+	renderer->GetDevice()->CreateBlendState(&state_desc, &state);
+
+	return state;
+}
