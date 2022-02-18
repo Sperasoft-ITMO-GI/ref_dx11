1d5f9a260247a7aca45a3ebab95873434b6fc5c6
Hochheilige <hochheilige01@gmail.com>
2022-02-18 18:13:35 +0300

    Create function in geometry shader, but it still works not quite right

--- shaders/Particles.hlsl
+++ shaders/Particles.hlsl
@@ -33,35 +33,20 @@ VSOut VSMain(VSIn IN)
 GeoOut CreateQuadVertex(VSOut data, float2 offset)
 {
     GeoOut o = (GeoOut) 0;
-    o.pos.xy += offset;
-    o.pos = mul(position_transform, o.pos);
+    float3 vert = data.pos;
+    vert.xz += offset;
+    o.pos = mul(position_transform, float4(vert, 1.0f));
+    o.color = data.color;
     return o;
 }
 
 [maxvertexcount(4)]
 void GSMain(point VSOut IN[1], inout TriangleStream<GeoOut> tristream)
 {
-    //tristream.Append(CreateQuadVertex(IN[0], float2(1, -1)));
-    //tristream.Append(CreateQuadVertex(IN[0], float2(1, 1)));
-    //tristream.Append(CreateQuadVertex(IN[0], float2(-1, -1)));
-    //tristream.Append(CreateQuadVertex(IN[0], float2(-1, 1)));
-    float3 v[4];
-    v[0] = float3(IN[0].pos + float3(1.0f, 0.0f, -1.0f));
-    v[1] = float3(IN[0].pos + float3(1.0f, 0.0f, 1.0f));
-    v[2] = float3(IN[0].pos + float3(-1.0f, 0.0f, -1.0f));
-    v[3] = float3(IN[0].pos + float3(-1.0f, 0.0f, 1.0f));
-    
-    
-    GeoOut OUT = (GeoOut) 0;
-    OUT.pos = mul(position_transform, float4(v[0], 1.0f));
-    OUT.color = IN[0].color;
-    tristream.Append(OUT);
-    OUT.pos = mul(position_transform, float4(v[1], 1.0f));
-    tristream.Append(OUT);
-    OUT.pos = mul(position_transform, float4(v[2], 1.0f));
-    tristream.Append(OUT);
-    OUT.pos = mul(position_transform, float4(v[3], 1.0f));
-    tristream.Append(OUT);
+    tristream.Append(CreateQuadVertex(IN[0], float2(1, -1)));
+    tristream.Append(CreateQuadVertex(IN[0], float2(1, 1)));
+    tristream.Append(CreateQuadVertex(IN[0], float2(-1, -1)));
+    tristream.Append(CreateQuadVertex(IN[0], float2(-1, 1)));
     tristream.RestartStrip();
 
 }
