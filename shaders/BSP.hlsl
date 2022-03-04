7cfdfb6efeeadb548c319257524bc5b29d4c4935
Hochheilige <hochheilige01@gmail.com>
2022-03-04 03:35:31 +0300

    Add bloom to bsp shader (need to use it in effects shader)

--- shaders/BSP.hlsl
+++ shaders/BSP.hlsl
@@ -5,8 +5,16 @@
 struct VSOut
 {
     float4 pos : SV_Position;
-    float2 texCoord : TEXCOORD;
+    float2 texCoord : TEXCOORD0;
     float2 lightmapCoord : LMTEXCOORD;
+    float2 tex0 : TEXCOORD1;
+    float2 tex1 : TEXCOORD2;
+    float2 tex2 : TEXCOORD3;
+    float2 tex3 : TEXCOORD4;
+    float2 tex4 : TEXCOORD5;
+    float2 tex5 : TEXCOORD6;
+    float2 tex6 : TEXCOORD7;
+    float2 tex7 : TEXCOORD8;
 };
 
 struct VSIn
@@ -16,6 +24,15 @@ struct VSIn
     float2 lightmapCoord : LmTexCoord;
 };
 
+static const float c0 = 1.0f;
+static const float c1 = 0.875f;
+static const float c2 = 0.75f;
+static const float c3 = 0.625f;
+static const float c4 = 0.5;
+static const float c5 = 0.375f;
+static const float c6 = 0.25f;
+static const float c7 = 0.125f;
+
 VSOut VSMain(VSIn IN)
 {
     VSOut OUT;
@@ -23,6 +40,16 @@ VSOut VSMain(VSIn IN)
     OUT.pos = mul(proj, float4(IN.pos.x, IN.pos.y, IN.pos.z, 1.0f));
     OUT.texCoord = IN.texCoord;
 	OUT.lightmapCoord = IN.lightmapCoord;
+    
+    OUT.tex0 = IN.texCoord + c0;
+    OUT.tex1 = IN.texCoord + c1;
+    OUT.tex2 = IN.texCoord + c2;
+    OUT.tex3 = IN.texCoord + c3;
+    OUT.tex4 = IN.texCoord + c4;
+    OUT.tex5 = IN.texCoord + c5;
+    OUT.tex6 = IN.texCoord + c6;
+    OUT.tex7 = IN.texCoord + c7;
+    
     return OUT;
 }
 
@@ -38,7 +65,24 @@ float4 PSMain(VSOut IN) : SV_Target
 	texColor *= lightmapTexture.Sample(Sampler, IN.lightmapCoord);
 #endif
 	
-	float4 result = texColor * model.color + float4(glow, 0.0f);
-
-    return result;
+    float4 result = texColor * model.color + float4(glow, 0.0f);
+    
+    float4 r0 = colorTexture.Sample(Sampler, IN.tex0);
+    float4 r1 = colorTexture.Sample(Sampler, IN.tex1);
+    r0 *= c0;
+    r0 += r1 * c1;
+    r1 = colorTexture.Sample(Sampler, IN.tex2);
+    float4 r2 = colorTexture.Sample(Sampler, IN.tex3);
+    r0 += r1 * c2;
+    r0 += r2 * c3;
+    r1 = colorTexture.Sample(Sampler, IN.tex4);
+    r2 = colorTexture.Sample(Sampler, IN.tex5);
+    r0 += r1 * c4;
+    r0 += r2 * c5;
+    r1 = colorTexture.Sample(Sampler, IN.tex6);
+    r2 = colorTexture.Sample(Sampler, IN.tex7);
+    r0 += r1 * c6;
+    r0 += r2 * c7;
+    
+    return result * r0;
 }
