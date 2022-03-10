0266ad73359b0630b40ae8b326a8e95ac1a30978
Hochheilige <hochheilige01@gmail.com>
2022-03-10 22:14:20 +0300

    add radius, intensity and threshold to bloom as constants need to create CB for this

--- shaders/Effects.hlsl
+++ shaders/Effects.hlsl
@@ -69,30 +69,30 @@ float4 PSMain(VSOut IN) : SV_Target
 #endif
     
 #ifdef BLOOM
-    float4 r0 = sceneTexture.Sample(Sampler, IN.tex0);
-    float4 r1 = sceneTexture.Sample(Sampler, IN.tex1);
+    float radius = 1.0f;
+    float4 r0 = sceneTexture.Sample(Sampler, IN.tex0) * radius;
+    float4 r1 = sceneTexture.Sample(Sampler, IN.tex1) * radius;
     r0 *= c0;
     r0 += r1 * c1;
-    r1 = sceneTexture.Sample(Sampler, IN.tex2);
-    float4 r2 = sceneTexture.Sample(Sampler, IN.tex3);
+    r1 = sceneTexture.Sample(Sampler, IN.tex2) * radius;
+    float4 r2 = sceneTexture.Sample(Sampler, IN.tex3) * radius;
     r0 += r1 * c2;
     r0 += r2 * c3;
-    r1 = sceneTexture.Sample(Sampler, IN.tex4);
-    r2 = sceneTexture.Sample(Sampler, IN.tex5);
+    r1 = sceneTexture.Sample(Sampler, IN.tex4) * radius;
+    r2 = sceneTexture.Sample(Sampler, IN.tex5) * radius;
     r0 += r1 * c4;
     r0 += r2 * c5;
-    r1 = sceneTexture.Sample(Sampler, IN.tex6);
-    r2 = sceneTexture.Sample(Sampler, IN.tex7);
+    r1 = sceneTexture.Sample(Sampler, IN.tex6) * radius;
+    r2 = sceneTexture.Sample(Sampler, IN.tex7) * radius;
     r0 += r1 * c6;
     r0 += r2 * c7;
-    
-    result = r0;
+    float intensity = 1.0f;
+    result = r0 * intensity;
     return result;
 #endif
     
 #ifdef SCENE
-    result = //sceneTexture.Sample(Sampler, IN.texCoord) 
-            bloomTexture.Sample(Sampler, IN.texCoord)
+    result = bloomTexture.Sample(Sampler, IN.texCoord)
            + effectTexture.Sample(Sampler, IN.texCoord);
 #endif
     
